3abb67aed9a6e732734455cb435208fe409e6670
Khaoua Mohamed <132424419+MesterSimo@users.noreply.github.com>
2025-08-22 15:13:39 +0100

    Create utils.py

---
+++ modules/utils.py
@@ -0,0 +1,9 @@
+# /modules/utils.py
+
+import modules.globals
+import modules.ui as ui
+
+def update_status(message: str, scope: str = 'DLC.CORE') -> None:
+    print(f'[{scope}] {message}')
+    if not modules.globals.headless:
+        ui.update_status(message)
